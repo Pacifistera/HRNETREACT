7f7c91dcd9f42c7096edf62a0e2f4ebf07ca37d2
MaxD <max.deroubaix@gmail.com>
2025-06-10 19:53:54 +0200

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/hrnet/' : '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
   test: {
     environment: 'jsdom',
     setupFiles: './src/setupTests.js',
